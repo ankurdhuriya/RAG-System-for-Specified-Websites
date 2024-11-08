cec592daaf249d426f2128033c721c2068db2a1d
Ankur Dhuriya <ankur.dhuriya@builder.ai>
2024-11-08 16:52:17 +0530

    change log level from debug to info

--- app/utils/log_config.py
+++ app/utils/log_config.py
@@ -4,7 +4,7 @@ import logging
 def setup_logging():
     """Set up logging configuration."""
     logging.basicConfig(
-        level=logging.DEBUG,
+        level=logging.INFO,
         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
         handlers=[
             logging.StreamHandler(),
